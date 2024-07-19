75e79a0fb24b56bffcfa977ff4a27413d8f8e923
unknown <jacolynlovins@gmail.com>
2024-07-19 00:57:46 -0500

    Edits

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ZPong
 {
@@ -9,12 +10,15 @@ namespace ZPong
         [SerializeField] private float startDelay = 3f;
         [SerializeField] private GameObject ballPrefab;
         [SerializeField] private GameObject canvasParent;
+        [SerializeField] private Vector3 entryPoint = new Vector3(0f, 920f, 0f);
 
         public Ball activeBall;
 
         public static GameManager Instance { get; private set; }
 
         private Goal[] goals;
+        public Image explosionImage;
+        private float explosionDuration = 20f;
 
         private void Awake()
         {
@@ -32,14 +36,42 @@ namespace ZPong
 
         void SetGame()
         {
+            // Clones the ball from the prefab at the start of the game
             if (activeBall == null)
             {
-                activeBall = Instantiate(ballPrefab, Vector3.zero, this.transform.rotation, canvasParent.transform)
+                activeBall = Instantiate(ballPrefab, entryPoint, this.transform.rotation, canvasParent.transform)
                     .GetComponent<Ball>();
-                activeBall.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                activeBall.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, 920f);
             }
 
+            StartCoroutine(FallIntoBall());
+        }
+
+        IEnumerator FallIntoBall()
+        {
             activeBall.SetBallActive(false);
+            float fallDuration = 1f;
+            float startTime = Time.time;
+
+            while (Time.time - startTime < fallDuration)
+            {
+                float t = (Time.time - startTime) / fallDuration;
+                activeBall.transform.position = Vector3.Lerp(entryPoint, Vector3.zero, t);
+                activeBall.GetComponent<RectTransform>().anchoredPosition = new Vector2(0f, Mathf.Lerp(920f, 0f, t));
+                yield return null;
+            }
+
+            activeBall.transform.position = Vector3.zero;
+            activeBall.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            activeBall.SetBallActive(true);
+
+            // Display the explosion effect
+            if (explosionImage != null)
+            {
+                explosionImage.gameObject.SetActive(true);
+                yield return new WaitForSeconds(explosionDuration);
+                explosionImage.gameObject.SetActive(false);
+            }
         }
 
         void StartGame()
